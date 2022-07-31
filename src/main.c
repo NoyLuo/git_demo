714ae2f01115e2eeb2b0bd1c292d8cab895abe8e
Noy <819355688@qq.com>
2022-07-31 20:10:53 +0800

    good luck with Noy Luo in Augest

--- src/main.c
+++ src/main.c
@@ -570,6 +570,7 @@ void main(void)
 	uint32_t connect_attempt = 0;
 
 	LOG_INF("The MQTT simple sample started");
+	LOG_INF("Good luck with you in Augest , Noy Luo");
 
 #if defined(CONFIG_MQTT_LIB_TLS)
 	err = certificates_provision();
