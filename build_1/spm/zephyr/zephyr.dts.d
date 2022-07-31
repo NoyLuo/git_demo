d7fe026ae54acd4047bf68a17065441f28508e81
macbook pro <jluo@MOBILOGIX.COM>
2022-07-31 16:07:29 +0800

    first commit

---
+++ build_1/spm/zephyr/zephyr.dts.d
@@ -0,0 +1,16 @@
+empty_file.o: /Users/macbookpro/work/NCS_SDK/zephyr/misc/empty_file.c \
+ /Users/macbookpro/work/NCS_SDK/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160.dts \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/arm/nordic/nrf9160_sica.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/common/mem.h \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/arm/nordic/nrf9160.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/arm/armv8-m.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/common/skeleton.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/include/dt-bindings/i2c/i2c.h \
+ /Users/macbookpro/work/NCS_SDK/zephyr/include/dt-bindings/gpio/gpio.h \
+ /Users/macbookpro/work/NCS_SDK/zephyr/include/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/arm/nordic/override.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/dts/arm/nordic/nrf9160_common.dtsi \
+ /Users/macbookpro/work/NCS_SDK/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common.dts \
+ /Users/macbookpro/work/NCS_SDK/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_partition_conf.dts \
+ /Users/macbookpro/work/NCS_SDK/nrf/samples/spm/nrf9160dk_nrf9160.overlay
